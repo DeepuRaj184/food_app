ff4f9dc62ddbc11dbad74eb30a685ac6149da593
DeepuRaj184 <141441321+DeepuRaj184@users.noreply.github.com>
2024-05-07 06:38:52 +0530

    Update food_context.js

--- src/context/food_context.js
+++ src/context/food_context.js
@@ -1,28 +1,36 @@
 import React, { useEffect, useState } from "react";
-import res_categories from "../data/restaurant_details";
+import res_categories from "../data/restaurant_details"; 
 import { createContext } from "react";
 
+// Creating a Context for managing food-related data
 export const foodContext = createContext();
 
-export const Myfood = ({children})=>{
+// Component to provide food Context to its children.,
+export const Myfood = ({ children }) => {
+    // State to store restaurant food data
+    const [resFood, setResFood] = useState([]);
 
-    const [resFood,setResFood] = useState([]);
-
-    useEffect(()=>{
+    // Effect to initialize resturant food data when the component mounts
+    useEffect(() => {
+        // Check if restaurant data exists and is not empty
         if (res_categories && res_categories.length > 0) {
+            // Set the restaurant food data
             setResFood(res_categories);
         }
-    },[])
+    }, []);
 
+    // Function to update filtered food based on category
     const updateFilteredFood = (category) => {
-            const filtered = res_categories.filter((item) => item.category === category);
-            setResFood(filtered);
+        // Filter the restaurant data based on the category
+        const filtered = res_categories.filter((item) => item.category === category);
+        // Update the restaurant food data with the filtered results
+        setResFood(filtered);
     };
 
-return(
-    <foodContext.Provider value={{resFood,updateFilteredFood}}>
-        {children}
-    </foodContext.Provider>
-)
-    
-}
+    // Provide the Restaurant food data and update function to its children Components through the context
+    return (
+        <foodContext.Provider value={{ resFood, updateFilteredFood }}>
+            {children}
+        </foodContext.Provider>
+    );
+};
